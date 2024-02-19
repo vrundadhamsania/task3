ad7afc3ff9306453242d58d17772c9c81cda18d6
vrundadhamsania <155124170+vrundadhamsania@users.noreply.github.com>
2024-02-19 09:13:26 +0530

    Update script_26.php

--- script_26.php
+++ script_26.php
@@ -1,5 +1,5 @@
 <?php
-$student=array("name"=>"prinsi","age"=>23,"city"=>"balapar");
+$student=array("name"=>"vrunda","age"=>23,"city"=>"upleta");
 print_r(array_push($student,"rajkot"));
 print_r($student);
 ?>
