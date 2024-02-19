4ea573a20e1756c2e3062484111e17ef2ee6e6ad
vrundadhamsania <155124170+vrundadhamsania@users.noreply.github.com>
2024-02-19 09:10:29 +0530

    Update script_19.php

--- script_19.php
+++ script_19.php
@@ -1,5 +1,5 @@
 <?php
-$student=array("name"=>"prinsi","age"=>23,"city"=>"balapar");
+$student=array("name"=>"vrunda","age"=>23,"city"=>"upleta");
 $value=array_values($student);
 print_r($value);
 ?>
