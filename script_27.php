eb76a8f5f0d9aab34ffbbf95a3dc698fc0851865
vrundadhamsania <155124170+vrundadhamsania@users.noreply.github.com>
2024-02-19 09:14:09 +0530

    Update script_27.php

--- script_27.php
+++ script_27.php
@@ -1,5 +1,5 @@
 <?php
-$student=array("name"=>"prinsi","age"=>23,"city"=>"balapar");
-$value=array("sname"=>"komal","sage"=>34,"scity"=>"balapar");
+$student=array("name"=>"vrunda","age"=>23,"city"=>"upleta");
+$value=array("sname"=>"yashvi","sage"=>34,"scity"=>"upleta");
 print_r(array_merge($student,$value));
 ?>
